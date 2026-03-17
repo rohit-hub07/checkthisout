d63b1e697f8c48f0b338c54218d154c5e9c0c7a4
rohitsingh <rohitsingh986491@gmail.com>
2026-03-18 01:27:39 +0530

    added rate limiting

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import { connection } from "./db.js";
+import { rateLimit } from 'express-rate-limit'
 import dotenv from "dotenv"
 import Data from "./db.model.js";
 
 dotenv.config();
 
 const app = express();
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
 const port = process.env.PORT;
 
